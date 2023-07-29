adb348fc8ea020d1af8c2b6367f3f689b5fa2204
puwshy <lisa.shevtsova@gmail.com>
2023-07-30 00:04:17 +0300

    first_commit

---
+++ palindrom.py
@@ -0,0 +1,15 @@
+
+vvod = input("Введите строку для проверки:")
+
+def check_palindrome(stroka):
+    reversed_str = stroka[::-1]
+    if reversed_str == stroka:
+        print('True')
+    else:
+        print('False')
+
+    
+check_palindrome(vvod)
+
+
+
